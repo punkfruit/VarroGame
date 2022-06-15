e358b36bb7c9f2af60f05a63923159ed166802dc
punkfruit <thecybergrape@gmail.com>
2022-06-14 19:26:02 -0600

    lanes

--- VarroUnity/Assets/scripts/UnitController.cs
+++ VarroUnity/Assets/scripts/UnitController.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
-
+    private Vector3 moveDirection;
+    public Rigidbody2D theRB;
     public float speed = 5;
+    public int lane;
+    public LaneMarkers lm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lm = FindObjectOfType<LaneMarkers>();
+        lane = Random.Range(0, lm.lanes.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         Vector3 pos = transform.position;
 
         pos.y += speed * Time.deltaTime; //to be changed later
+        //pos.x += lm.lanes[lane].position.x;
+        //pos.MoveTowards(transform.position, lm.lanes[lane].transform, speed * Time.deltaTime);
+        if(transform.position.x != lm.lanes[lane].position.x)
+
+        pos.Normalize();
         transform.position = pos;
+        */
+
+        moveDirection.x = lm.lanes[lane].position.x - transform.position.x;
+        moveDirection.y += speed * Time.deltaTime;
+
+        moveDirection.Normalize();
+
+        theRB.velocity = moveDirection * speed;
+
     }
 
     public void UnitDeath()
