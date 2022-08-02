d1a9376704b22d573a7af1f033a897c4fa9774e7
punkfruit <thecybergrape@gmail.com>
2022-08-02 04:24:57 -0600

    stuff2

--- VarroUnity/Assets/scripts/UnitSelectorButton.cs
+++ VarroUnity/Assets/scripts/UnitSelectorButton.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class UnitSelectorButton : MonoBehaviour
 {
+    public GameObject unit;
+    public GameObject highlight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        highlight.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseEnter()
+    {
+        highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
     }
 }
