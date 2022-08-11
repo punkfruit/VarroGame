8a0680d026dea492b07f969b981d352b352acd05
punkfruit <thecybergrape@gmail.com>
2022-08-10 22:46:51 -0600

    ddd

--- VarroUnity/Assets/scripts/UnitController.cs
+++ VarroUnity/Assets/scripts/UnitController.cs
@@ -11,6 +11,10 @@ public class UnitController : MonoBehaviour
     public LaneMarkers lm;
 
     public int health = 100;
+    public bool moveTowardEnemyBase = true;
+
+
+    public GameObject turret;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,17 +25,37 @@ public class UnitController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (moveTowardEnemyBase)
+        {
+            MoveTowardEnemyBase();
+        }
+            
+    }
+
+    public void UnitDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    public void MoveTowardEnemyBase()
+    {
         /*
-        Vector3 pos = transform.position;
+       Vector3 pos = transform.position;
 
-        pos.y += speed * Time.deltaTime; //to be changed later
-        //pos.x += lm.lanes[lane].position.x;
-        //pos.MoveTowards(transform.position, lm.lanes[lane].transform, speed * Time.deltaTime);
-        if(transform.position.x != lm.lanes[lane].position.x)
+       pos.y += speed * Time.deltaTime; //to be changed later
+       //pos.x += lm.lanes[lane].position.x;
+       //pos.MoveTowards(transform.position, lm.lanes[lane].transform, speed * Time.deltaTime);
+       if(transform.position.x != lm.lanes[lane].position.x)
 
-        pos.Normalize();
-        transform.position = pos;
-        */
+       pos.Normalize();
+       transform.position = pos;
+       */
 
         moveDirection.x = lm.lanes[lane].position.x - transform.position.x;
         moveDirection.y += speed * Time.deltaTime;
@@ -39,11 +63,5 @@ public class UnitController : MonoBehaviour
         moveDirection.Normalize();
 
         theRB.velocity = moveDirection * speed;
-
-    }
-
-    public void UnitDeath()
-    {
-        Destroy(gameObject);
     }
 }
